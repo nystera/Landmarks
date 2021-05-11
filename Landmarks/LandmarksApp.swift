372b3189b7a9cf47a9357308c70bea062e78daec
Nathan Yeo <42372568+nystera@users.noreply.github.com>
2021-05-11 09:18:34 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nathan Yeo on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
